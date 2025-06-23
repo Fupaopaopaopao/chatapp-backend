18b4e24546464731401b29efe94f8ffb8af65ae6
Mac <fupaopao8483@gmail.com>
2025-06-24 02:24:51 +1000

    simple api and connect with mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import express from "express"
-
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import {connectDB} from "./lib/db.js"
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT;
 
-app.listen(5001,()=>{
-    console.log("server is running on port 5001")
-})
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("server is running on port "+PORT);
+  connectDB();
+});
